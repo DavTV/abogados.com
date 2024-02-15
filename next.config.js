b1ee1e21418eca7e35f0439caad91434bc2dd2f8
DavTV <atellovalqui@gmail.com>
2024-02-15 15:40:14 -0500

    Agregando rutas remotas

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns:[
+    // Configuración para entorno de desarrollo (local)
+    // Aquí se utiliza localhost:1337 para las imágenes en el entorno local
+    // Asegúrate de ajustar el puerto y el hostname según tu configuración de desarrollo
+    domains: ['localhost:1337'],
+    // Configuración para entorno de producción (servidor remoto)
+    // Aquí puedes agregar el dominio de tu servidor remoto para las imágenes en producción
+    // Asegúrate de ajustar el protocolo, hostname y cualquier otro detalle según tu configuración de producción
+    remotePatterns: [
       {
-      protocol:"http",
-      hostname:"localhost",
-      port:"1337",
-      pathname:"/**"
-      }  
-    ] // Agrega el dominio de tu API o servidor de imágenes aquí
-  },
+        protocol: "https",
+        hostname: "https://abogadosdb.onrender.com", // Reemplaza "tudominio.com" con tu dominio real
+        pathname: "/**"
+      }
+    ]
+  }
 };
